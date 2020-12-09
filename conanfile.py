de4e2f86bad5b903a14aba187fffc5a828ca466e
Anders Wind <anders.wind@alipescapital.com>
2020-12-09 16:14:11 +0100

    Setting msvc runtime correctly and added option for building test

--- conanfile.py
+++ conanfile.py
@@ -3,21 +3,33 @@ from conans import ConanFile, CMake
 
 class VcConan(ConanFile):
     name = "Vc"
-    version = "3fbc69cc"
+    version = "553abc7c"
     license = "3-clause BSD license"
     author = "Matthias Kretz M.Kretz@gsi.de"
     url = "https://github.com/VcDevel/Vc"
     description = "SIMD Vector Classes for C++ "
     topics = ("c++", "simd", "vectorization")
     settings = "os", "compiler", "build_type", "arch"
-    options = {}
-    default_options = {}
+    options = {"VC_BUILD_TESTING": [True, False]}
+    default_options = {"VC_BUILD_TESTING": False}
     generators = "cmake"
     exports_sources = "*"
 
     def build(self):
+        # pylint: disable=no-member
+
         cmake = CMake(self)
         cmake.definitions["BUILD_TESTING"] = False
+        if self.settings.compiler == "Visual Studio":
+            runtimes = {
+                'MT': 'MultiThreaded',
+                'MTd': 'MultiThreadedDebug',
+                'MD': 'MultiThreadedDLL',
+                'MDd': 'MultiThreadedDebugDLL',
+            }
+            cmake.definitions["CMAKE_MSVC_RUNTIME_LIBRARY"] = runtimes[str(
+                self.settings.compiler.runtime)]
+
         cmake.configure(source_folder=".", build_folder="./build")
         cmake.build()
         cmake.install()
